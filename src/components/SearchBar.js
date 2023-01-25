03039836f12fd40b3379c81245a5424df2bd615f
neeraj vijay <neeraj.vijay@deepfleet.com>
2023-01-25 21:28:04 +0530

    code cleaned

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,8 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCoinList } from '../redux/action/action';
+
+
 
 export default function SearchBar() {
-  const dispatch = useDispatch();
-
-
-
-
-  useEffect(() => {
-
-    dispatch(fetchCoinList());
-
-
-  }, []);
-
-  const exchangeData = useSelector((state) => state.exchange);
-
-  console.log(exchangeData.coinList)
 
   return (
     <div>SearchBar</div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,8 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { sideList } from '../redux/action/action';
+
+
 
 export default function Sidebar() {
-
-
-  const dispatch = useDispatch();
-
-
-
-
-  useEffect(() => {
-
-    dispatch(sideList());
-
-
-  }, []);
-
-  const data = useSelector((state) => state.sidelist);
-
-  console.log(data.coin)
-
 
   return (
     <div>Sidebar</div>
